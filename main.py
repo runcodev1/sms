dcb51ae1783fa11cad18aa4bcfb8c25a9de06028
runcodev1 <runcodev1@gmail.com>
2026-01-30 17:45:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -37,15 +37,8 @@ import datetime
 avatarbot = "https://media.discordapp.net/attachments/1173589548152926228/1201018181876199564/standard.gif?ex=65c84a58&is=65b5d558&hm=a90543158db13e39ff1c70b303ff4d9bc12367e51c437e54969af80d4c6c2979&=&width=288&height=288"
 Alert = "> ⚠️ คุณไม่มีสิทธิ์ หรือ การอนุณาติที่สามารถใช้คำสั่งนี้ได้คะ "
 
+LOGCHANNEL = 1463520487249547386
 TOKEN = os.getenv("DISCORD_TOKEN")
-LOGCHANNEL = int(os.getenv("LOGCHANNEL_ID"))
-channel = client.get_channel(LOGCHANNEL)
-if channel is None:
-    await interaction.followup.send(
-        "❌ ไม่พบห้อง LOGCHANNEL กรุณาตรวจสอบ LOGCHANNEL_ID",
-        ephemeral=True
-    )
-    return
 
 X = 50 # จำนวนทั้งหมด
 class sms_button(discord.ui.View):
@@ -141,7 +134,4 @@ async def setupsms(interaction: discord.Interaction, error):
 
 
 client.run(TOKEN)
-
-
-
 
