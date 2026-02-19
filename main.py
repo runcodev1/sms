4b733c416b97f166aeb6b2b28e9d9d556a523fae
runcodev1 <runcodev1@gmail.com>
2026-02-19 12:25:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ import datetime
 
 avatarbot = "https://media.discordapp.net/attachments/1173589548152926228/1201018181876199564/standard.gif?ex=65c84a58&is=65b5d558&hm=a90543158db13e39ff1c70b303ff4d9bc12367e51c437e54969af80d4c6c2979&=&width=288&height=288"
 Alert = "> ⚠️ คุณไม่มีสิทธิ์ หรือ การอนุณาติที่สามารถใช้คำสั่งนี้ได้คะ "
-LOGCHANNEL = 1211945009662795787 # IC CAHNNEL ต้องมี
+LOGCHANNEL = 1473913189845696623 # IC CAHNNEL ต้องมี
 TOKEN = os.getenv("TOKEN") # โทเค้นบอทดิสคอร์ด
 X = 50 # จำนวนทั้งหมด
 class sms_button(discord.ui.View):
@@ -132,5 +132,6 @@ async def setupsms(interaction: discord.Interaction, error):
 
 
 client.run(TOKEN)
+
 
 
